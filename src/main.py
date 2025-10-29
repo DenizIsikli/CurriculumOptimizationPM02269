f0f4f627e5d42c872b50974915da1031fbc17675
Deniz Isikli <s215818@dtu.dk>
2025-10-29 22:09:06 +0100

    Empty for now

--- src/main.py
+++ src/main.py
@@ -1,13 +1,12 @@
-from src.data_preparation import prepare_data
+from src import config
+from src.data_preparation import DataPreparer
 from src.process_discovery import discover_process
 from src.performance_analysis import analyze_performance
 from src.conformance_checking import check_conformance
 
 def main():
-    event_log = prepare_data()
-    model = discover_process(event_log)
-    analyze_performance(event_log, model)
-    check_conformance(event_log, model)
+    pass
+
 
 if __name__ == "__main__":
     main()
