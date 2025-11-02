35e9c6a3c889649b23e4d7b492db9ca5014c1b86
Deniz Isikli <s215818@dtu.dk>
2025-11-02 23:18:26 +0100

    config

--- src/config.py
+++ src/config.py
@@ -9,7 +9,7 @@ PROCESSED_DATA_PATH = os.path.join(DATA_PATH, "processed_log.csv")
 SAMPLED_DATA_PATH = os.path.join(DATA_PATH, "sampled_log.csv")
 XES_OUTPUT_PATH = os.path.join(DATA_PATH, "sampled_event_log.xes")
 
-SAMPLE_FRACTION = 0.2
+SAMPLE_FRACTION = 0.05  # 5% sample for quicker processing
 
 # ======================================
 # model_training.py Configurations
@@ -23,7 +23,17 @@ PTREE_PATH = os.path.join(MODEL_PATH, "process_tree.png")
 IM_IMAGE_PATH = os.path.join(MODEL_PATH, "inductive_miner.png")
 HM_IMAGE_PATH = os.path.join(MODEL_PATH, "heuristics_miner.png")
 
+# ======================================
+# performance_analysis.py Configurations
+# ======================================
+PERFORMANCE_PATH = os.path.join("performance_analysis")
+PERFORMANCE_LOG_PATH = os.path.join(PERFORMANCE_PATH, "performance_log.txt")
 
+
+# ======================================
+# Create necessary directories
+# ======================================
 # Ensure directories exist
 os.makedirs(DATA_PATH, exist_ok=True)
 os.makedirs(MODEL_PATH, exist_ok=True)
+os.makedirs(PERFORMANCE_PATH, exist_ok=True)
