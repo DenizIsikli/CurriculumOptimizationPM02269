f23775aaba5deaad80e0534ca474bbf26f490d7f
Deniz Isikli <s215818@dtu.dk>
2025-11-05 01:25:39 +0100

    Descriptive fraction log

--- src/process_discovery.py
+++ src/process_discovery.py
@@ -6,7 +6,7 @@ from pm4py.statistics.traces.generic.log import case_statistics
 from pm4py.objects.log.importer.xes import importer as xes_importer
 from pm4py.objects.petri_net.exporter import exporter as pnml_exporter
 
-from config import MODEL_PATH, SAMPLE_FRACTION, XES_OUTPUT_PATH
+from src.config import MODEL_PATH, SAMPLE_FRACTION, XES_OUTPUT_PATH
 
 
 class ProcessDiscovery:
@@ -30,7 +30,10 @@ class ProcessDiscovery:
         self._save_process_tree()
 
     def _fraction_log(self):
-        n = int(len(self.event_log) * self.sample_fraction)
+        total_traces = len(self.event_log)
+        n = int(total_traces * self.sample_fraction)
+        print(f"Total unique students before sampling: {total_traces}")
+        print(f"Sampling {n} traces (~{self.sample_fraction*100:.1f}%) of total")
         self.event_log = sampling.sample_log(self.event_log, n)
 
     def _summarize_log(self) -> None:
